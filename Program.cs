bb4c3d90050123e0418cd3a1ced944060548b0c7
Johnathan Calle <jmcalle@udemedellin.edu.co>
2023-08-28 21:33:01 -0500

    Pastel Asserts...

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
             LibrarySystem librarySystem = new LibrarySystem();
             librarySystem.AddBook("The Catcher in the Rye", "J.D. Salinger", "9780316769174");
             Console.WriteLine(librarySystem.GetAvailableBooksCount());
+
+            //Pastel Assert --> https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert?view=visualstudiosdk-2022
         }
     }
 }
